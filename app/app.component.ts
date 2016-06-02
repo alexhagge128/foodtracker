06fb410f2c81c2b3b22480db8dc89f196b3b873b
Alex Hagge and Ned Faulhaber <Guest@epicodus-54.local>
2016-06-02 12:11:29 -0700

    added emitter functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,48 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 
 @Component({
+  selector: 'keg-list',
+  inputs: ['kegList'],
+  outputs: ['onKegSelect'],
+
+  template: `
+    <div *ngFor="#currentKeg of kegList" (click)="kegClicked(currentKeg)">
+      <h3> {{currentKeg.brand}} </h3>
+      <h4>Type: {{currentKeg.name}}</h4>
+      <h4>Price: $ {{currentKeg.price}}</h4>
+      <h4>ABV: {{currentKeg.abv}}</h4>
+      <h4 (click)="kegWasTapped(keg)">Pints Left: {{currentKeg.pintCount}}</h4>
+    </div>
+  `
+})
+
+export class KegListComponent {
+  public kegList: Keg[];
+  public onKegSelect: EventEmitter<Keg>;
+  constructor(){
+    this.onKegSelect = new EventEmitter();
+  }
+  kegClicked(clickedKeg: Keg): void {
+    console.log('child', clickedKeg);
+    this.onKegSelect.emit(clickedKeg);
+  }
+
+  // kegWasTapped(tappedKeg: Keg): void {
+  //   tappedKeg.pintCount -= 1;
+  // }
+}
+
+//------AppComponent----------------------
+@Component({
   selector: 'my-app',
+  directives: [KegListComponent],
   template: `
   <div class="container">
     <h1>Kegs on Tap</h1>
-      <div *ngFor="#keg of kegs">
-        <h3> {{keg.brand}} </h3>
-        <h4>Type: {{keg.name}}</h4>
-        <h4>Price: $ {{keg.price}}</h4>
-        <h4>ABV: {{keg.abv}}</h4>
-        <h4>Pints Left: {{keg.pintCount}}</h4>
-      </div>
+    <keg-list
+      [kegList]="kegs"
+      (onKegSelect)="kegWasSelected($event)">
+    </keg-list>
   </div>
   `
 })
@@ -24,10 +55,12 @@ export class AppComponent {
       new Keg("IPA", "Terminal Gravity", 5, 7, 124),
     ];
   }
+  kegWasSelected(clickedKeg: Keg): void {
+    console.log(clickedKeg);
+  }
 }
 
-//------------------------
-
+//-----------Keg Model--------------
 export class Keg {
   constructor(public name: string, public brand: string, public price: number, public abv: number, public pintCount: number) {
 
